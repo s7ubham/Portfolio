a69c69917fba0bc9f44526492b351ccdbe6f723e
Subham <sspanda1700@gmail.com>
2026-02-15 02:59:04 +0530

    Fix vite config for ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@styles': path.resolve(__dirname, './src/styles'),
+      '@': new URL('./src', import.meta.url).pathname,
+      '@components': new URL('./src/components', import.meta.url).pathname,
+      '@pages': new URL('./src/pages', import.meta.url).pathname,
+      '@hooks': new URL('./src/hooks', import.meta.url).pathname,
+      '@utils': new URL('./src/utils', import.meta.url).pathname,
+      '@types': new URL('./src/types', import.meta.url).pathname,
+      '@styles': new URL('./src/styles', import.meta.url).pathname,
     },
   },
 })
