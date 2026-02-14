c86442542cc91b2e2d51754d84c7a31d0adf1fd7
Subham <sspanda1700@gmail.com>
2026-02-15 03:00:15 +0530

    Simplify vite config paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
-      '@components': new URL('./src/components', import.meta.url).pathname,
-      '@pages': new URL('./src/pages', import.meta.url).pathname,
-      '@hooks': new URL('./src/hooks', import.meta.url).pathname,
-      '@utils': new URL('./src/utils', import.meta.url).pathname,
-      '@types': new URL('./src/types', import.meta.url).pathname,
-      '@styles': new URL('./src/styles', import.meta.url).pathname,
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@hooks': '/src/hooks',
+      '@utils': '/src/utils',
+      '@types': '/src/types',
+      '@styles': '/src/styles',
     },
   },
 })
